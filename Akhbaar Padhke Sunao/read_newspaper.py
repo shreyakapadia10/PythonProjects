52ba86eb80c09020ecdbc65e2344db5e949a34d3
Shreya Kapadia <shreyakapadia8@gmail.com>
2021-02-20 12:27:03 +0530

    Added read_newspaper.py

---
+++ Akhbaar Padhke Sunao/read_newspaper.py
@@ -0,0 +1,27 @@
+import requests
+import json
+from num2words import num2words
+
+
+def speak(string):
+    from win32com.client import Dispatch
+    speaker = Dispatch("SAPI.SpVoice")
+    speaker.Speak(string)
+
+
+if __name__ == '__main__':
+    url = ('http://newsapi.org/v2/top-headlines?'
+           'country=in&'
+           'apiKey=7efce32c119740c0ab8d11bd4a1014cc')
+    response = requests.get(url)
+    news = response.text
+    json_data = json.loads(news)
+
+    for i in range(1, 11):
+        speak(f"The {num2words(i, ordinal='ordinal_num')} "
+              f"News is from the source: {json_data['articles'][i]['source']['name']}"
+              f"The News Title is: {json_data['articles'][i]['title']}"
+              f"The News Description is: {json_data['articles'][i]['description']}"
+              f"You can read entire news from URL printed in console")
+
+        print(f"You can read entire news from URL: {json_data['articles'][i]['url']}")
