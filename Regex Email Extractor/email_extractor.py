06635ea56a55fdfc158e31d0d14568f1153614d9
Shreya Kapadia <shreyakapadia8@gmail.com>
2021-02-21 12:40:30 +0530

    Made some changes in email pattern in email_extractor.py

--- Regex Email Extractor/email_extractor.py
+++ Regex Email Extractor/email_extractor.py
@@ -7,19 +7,20 @@ to:	shreyad8@gmail.com
 date:	Feb 20, 2021, 12:22 PM
 subject:	Thank you for registering to our event!
 mailed-by:	em670.wixevents.@com.com
-signed-by:	wixevents.com
+signed-by:	wixevents@edu.com
+wixevents@edu..com
 security:	 Standard encryption (TLS) Learn more
 :	Important according to Google magic.
 '''
 
-pattern = re.compile(r'\w{2,}(\.)*\w{1,}@[A-Za-z]{2,}.[A-Za-z]{2,3}')
+pattern = re.compile(r'\w{2,}(\.)*\w{1,}@[A-Za-z]{2,}.[A-Za-z]{2,3}(\.){0,1}[A-Za-z]{0,3}')
 emails_re = pattern.finditer(mystr)
 
 emails = []
 
 for email in emails_re:
     emails.append(f'{email.group()} \n')
-print(emails)
+# print(emails)
 with open('emails_lst.txt', 'a') as fp:
     for mail in emails:
         fp.write(mail)
